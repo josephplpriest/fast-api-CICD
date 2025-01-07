5fd815ec349422c9975dd8314bb66957b657c7b0
Paul Priest <Josephplpriest@gmail.com>
2025-01-07 14:38:16 -0500

    fixed lint error

--- app/main.py
+++ app/main.py
@@ -27,4 +27,8 @@ def read_item(item_id: int, q: Union[str, None] = None):
 
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id, "Has Offer": item.is_offer}
+    return {
+        "item_name": item.name,
+        "item_id": item_id,
+        "Has Offer": item.is_offer
+    }
